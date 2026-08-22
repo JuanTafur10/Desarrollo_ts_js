a6d40ab5d4776af1e1ecb17e014d469ba3ac095f
JuanTafur10 <jtafur166@soyudemedellin.edu.co>
2026-08-21 21:35:52 -0500

    Se hace el ejercicio 7

---
+++ ejercicios-ts/ejercicio-7.ts
@@ -0,0 +1,9 @@
+const persona = {
+    nombre: "Juan Pablo",
+    edad: 19,
+    ciudad: "Ibagué"
+};
+
+console.log("Nombre:", persona.nombre);
+console.log("Edad:", persona.edad);
+console.log("Ciudad:", persona.ciudad);
